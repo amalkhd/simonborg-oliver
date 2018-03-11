95cd5ff1f6c43cc418f5bcd4afe19b61e70eb501
amalkriz <amalkhd@gmail.com>
2018-03-11 19:38:45 +0530

    added pre loader

--- layout/header.php
+++ layout/header.php
@@ -17,9 +17,10 @@
 </head>
 
 <body>
-  <!-- <div id="pre-loader" class="pre-loader">
-    <img src="" alt="">
-  </div> -->
+  <div class="pre-loader">
+    <svg class="spinner" width="65px" height="65px" viewBox="0 0 66 66" xmlns="http://www.w3.org/2000/svg">
+     <circle class="path" fill="none" stroke-width="6" stroke-linecap="round" cx="33" cy="33" r="30"></circle>
+  </div>
   <div class="container-fluid">
     <nav id="header" class="navbar navbar-fixed-top header" role="navigation">
       <div class="container">
